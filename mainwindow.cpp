4a3a2cd77645592647bf9bfc54cd6d496fdd98b0
Petr Savichev (proton) <psavichev@gmail.com>
2021-03-10 04:25:22 -0500

    open file in command line argument

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFile>
+#include <QTextStream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->textEdit->setPlainText("aaa");
+
+    auto filePath = QCoreApplication::arguments().at(1);
+
+    QFile file(filePath);
+    file.open(QIODevice::ReadOnly);
+
+    QTextStream in(&file);
+    auto content = in.readAll();
+    file.close();
+
+    ui->textEdit->setPlainText(content);
 }
 
 MainWindow::~MainWindow()
